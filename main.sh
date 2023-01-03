203310790df27a07f12d3e133e0c7858b9560c27
MohamedAl-Attar <mohamedelattar28@yahoo.com>
2023-01-03 22:55:01 +0200

    Main With GUI

--- main.sh
+++ main.sh
@@ -5,103 +5,172 @@ shopt -s extglob
 
 export pat="^[a-zA-Z]+[a-zA-Z0-9[:space:]]*"
 
-# DBpath=$PWD
-# cd ..
+DBpath=$PWD
+cd ..
 
-# if [ -d "$PWD/Database" ]; then
-#     echo "im here1"
-#     cd "$PWD/Database" || exit
-# else
-# echo "im here2"
-# cd "$DBpath" || exit
 if [ -d "$PWD/Database" ]; then
+    echo "im here1"
     cd "$PWD/Database" || exit
 else
-    mkdir "$PWD/Database"
-    cd "$PWD/Database" || exit
+    echo "im here2"
+    cd "$DBpath" || exit
+    if [ -d "$PWD/Database" ]; then
+        cd "$PWD/Database" || exit
+    else
+        mkdir "$PWD/Database"
+        cd "$PWD/Database" || exit
+    fi
 fi
-# fi
 
 showMenu() {
-    select choice in createDatabase listDatabase removeDatabase connectDatabase Exit; do
+    choice=$(zenity --list \
+      --column "Select Menu" \
+      createDatabase \
+      listDatabase \
+      removeDatabase \
+      connectDatabase \
+      Exit
+      )
+    #select choice in createDatabase listDatabase removeDatabase connectDatabase Exit; do
         case $choice in
         createDatabase)
             createDB
             ;;
         listDatabase)
-            echo "Databases found:"
+            zenity --info \
+                        --title "Info Message" \
+                        --width 500 \
+                        --height 100 \
+                        --text "Databases found: 
+`ls -d */`"
+            #echo "Databases found:"
             # ls -F | grep /
-            # ls -d */
-            res=$(ls -d */ | awk 'BEGIN{FS=" "} { print "- "$1 }')
-            if [[ $res == "" ]]; then
-                echo "No Databases found"
-            else
-                echo $res
-            fi
+            #ls -d */
             # showChoices
             showMenu
             ;;
         removeDatabase)
-            echo "removeDatabase"
-            read -r -p "Enter a name for the database: " name
+            #echo "removeDatabase"
+            name=$(zenity --entry \
+                --width 500 \
+                --title "check user" \
+                --text "Enter the user name");
+            #read -r -p "Enter a name for the database: " name
             if [ -d "$name" ]; then
-                read -r -p "You are going to drop $name? Are you sure? [Y/N]: " check
-                if [[ $check = "Y" || $check = "y" ]]; then
+                zenity --question \
+                    --title "Info Message" \
+                    --width 500 \
+                    --height 100 \
+                    --text "You are going to drop $name? Are you sure? [Y/N]: " 
+                #read -r -p "You are going to drop $name? Are you sure? [Y/N]: " check
+                #if [[ $check = "Y" || $check = "y" ]]; then
                     sudo rm -r "$name"
-                    echo "$name Database removed successfully!"
-                fi
+                    zenity --info \
+                        --title "Info Message" \
+                        --width 500 \
+                        --height 100 \
+                        --text "$name Database removed successfully!"
+                    #echo "$name Database removed successfully!"
+                #fi
             else
-                echo "$name database is not existed!"
+                zenity --info \
+                        --title "Info Message" \
+                        --width 500 \
+                        --height 100 \
+                        --text "$name database is not existed!"
+                #echo "$name database is not existed!"
             fi
             # showChoices
             showMenu
             ;;
         connectDatabase)
-            pwd
             echo "connectDatabase"
-            read -r -p "Enter a name for the database: " name
+            name=$(zenity --entry \
+                --width 500 \
+                --title "check user" \
+                --text "Enter a name for the database: " );
+            #read -r -p "Enter a name for the database: " name
             if [ -d "$name" ]; then
                 cd "$name" || exit
-                echo "Connected to $name database"
-                . ../../connectDB.sh
+                zenity --info \
+                        --title "Info Message" \
+                        --width 500 \
+                        --height 100 \
+                        --text "Connected to $name database"
+                #echo "Connected to $name database"
+                . connectDB.sh
             else
-                echo "$name database not exist,try another name"
+                zenity --info \
+                        --title "Info Message" \
+                        --width 500 \
+                        --height 100 \
+                        --text "$name database not exist,try another name"
+                #echo "$name database not exist,try another name"
             fi
             # main.sh
             showMenu
             ;;
-
+            
         Exit)
             echo "Exit"
             # break
             exit
             ;;
         *)
-            echo "wrong input select again"
+            zenity --info \
+                --title "Info Message" \
+                --width 500 \
+                --height 100 \
+                --text "wrong input select again"
+            #echo "wrong input select again"
             showMenu
             ;;
         esac
-    done
+    #done
 }
 
 createDB() {
 
     echo "createDatabase"
-    read -r -p "Enter a name for the database: " name
-    echo "$name"
+    name=$(zenity --entry \
+                --width 500 \
+                --title "check user" \
+                --text "Enter a name for the database: " );
+    #read -r -p "Enter a name for the database: " name
+    #echo "$name"
     if [[ -n $name ]]; then
         if [[ $name =~ $pat ]]; then
             if [[ -d "$name" ]]; then
-                echo 'Database already exists,try another name'
+                zenity --info \
+                        --title "Info Message" \
+                        --width 500 \
+                        --height 100 \
+                        --text "Database already exists,try another name"
+                #echo 'Database already exists,try another name'
             else
                 mkdir "$name"
-                echo "$name" database created succsessfully!!!
+                zenity --info \
+                        --title "Info Message" \
+                        --width 500 \
+                        --height 100 \
+                        --text "$name database created succsessfully!!!"
+                #echo "$name database created succsessfully!!!"
             fi
         else
-            echo 'Error naming the file, make sure to not start with number,special char and space,try another name'
+            zenity --info \
+                        --title "Info Message" \
+                        --width 500 \
+                        --height 100 \
+                        --text "Error naming the file, make sure to not start with number,special char and space,try another name"
+            #echo 'Error naming the file, make sure to not start with number,special char and space,try another name'
         fi
     else
-        echo "zero string,try another name"
+        zenity --info \
+                        --title "Info Message" \
+                        --width 500 \
+                        --height 100 \
+                        --text "zero string,try another name"
+        #echo "zero string,try another name"
     fi
     # showChoices
     # cd ..
@@ -110,6 +179,12 @@ createDB() {
 }
 
 showMenu
+
+
+
+
+
+
 
 #  createTable listTable dropTable insertTable selectTable removeTable updateTable Exit;
 
